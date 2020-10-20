c30c7c56b85ea2cd48ea4921db2027f132708d78
Fred <fpaula@gmail.com>
2020-10-19 21:18:14 -0300

    Return json

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,11 @@
 import io.javalin.Javalin
+import model.Person
 
 fun main(args: Array<String>) {
     val app = Javalin.create().start(7000)
-    app.get("/") { ctx -> ctx.result("Hello World") }
+    app.get("/person/:id") {
+        var person = Person(it.pathParam("id").toInt(), "Fred", 39)
+        it.contentType("application/json")
+        it.json(person)
+    }
 }
